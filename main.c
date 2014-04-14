f6dcce93c73189dd9dd136a6ce8878edc28807e6
Justin Crawford <Justasic@Gmail.com>
2014-04-14 15:45:38 -0700

    Add some more opcodes, change how the interpreter interprets them, this offsets the instruction set (making the existing program noops)

--- main.c
+++ main.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
  
+// 3 registers for us to use
 #define NUM_REGS 4
-unsigned regs[ NUM_REGS ];
+unsigned regs[NUM_REGS];
+
+// OP codes
+enum
+{
+	OP_UNUSED = 0,
+	OP_NOOP   = 0x01,
+	OP_HALT   = 0x02,
+	OP_LOADI  = 0x03,
+	OP_ADD    = 0x04,
+	OP_SUB    = 0x05,
+	OP_DIV    = 0x06
+};
+
+// Op code tables
+typedef struct
+{
+	int opcode;
+	const char *opcodename;
+} opcodes_t;
+
+static const opcodes_t OpTable[] = {
+	{OP_UNUSED, "Unused"},
+	{OP_NOOP,   "NOOP"},
+	{OP_HALT,   "HALT"},
+	{OP_LOADI,  "LOADI"},
+	{OP_ADD,    "ADD"},
+	{OP_SUB,    "SUB"},
+	{OP_DIV,    "DIV"}
+};
  
-unsigned program[] = { 0x1064, 0x11C8, 0x2201, 0x0000 };
+unsigned program[] =
+{
+	0x1064,    // loadi r0, 100
+	0x11C8,    // loadi r1, 200
+	0x2201,    // add r2, r0, r1
+	0x0000     // halt
+};
  
 /* program counter */
 int pc = 0;
@@ -11,7 +47,7 @@ int pc = 0;
 /* fetch the next word from the program */
 int fetch()
 {
-    return program[ pc++ ];
+    return program[pc++];
 }
  
 /* instruction fields */
@@ -37,26 +73,38 @@ int running = 1;
 /* evaluate the last decoded instruction */
 void eval()
 {
-    switch(instrNum)
-    {
-        case 0:
-            /* halt */
-            printf("halt\n");
-            running = 0;
-            break;
-        case 1:
-            /* loadi */
-            printf("loadi r%d #%d\n", reg1, imm);
-            regs[reg1] = imm;
-            break;
-        case 2:
-            /* add */
-            printf("add r%d r%d r%d\n", reg1, reg2, reg3);
-            regs[reg1] = regs[reg2] + regs[reg3];
-            break;
-        default:
-            printf("Unknown instruction %d\n", instrNum);
-    }
+	printf("%s {r0: %d, r1: %d, r2: %d}\n", OpTable[instrNum].opcodename, reg1, reg2, reg3);
+	switch(instrNum)
+	{
+		case OP_UNUSED:
+			// ignore but print warning
+			printf("Unused opcode encountered... Ignoring!\n");
+		case OP_NOOP:
+			// No-Operation
+			break;
+		case OP_HALT:
+			/* halt */
+			running = 0;
+			break;
+		case OP_LOADI:
+			/* loadi */
+		// 		printf("loadi r%d #%d\n", reg1, imm);
+			regs[reg1] = imm;
+			break;
+		case OP_ADD:
+			/* add */
+		// 		printf("add r%d r%d r%d\n", reg1, reg2, reg3);
+			regs[reg1] = regs[reg2] + regs[reg3];
+			break;
+		case OP_SUB:
+			regs[reg1] = regs[reg2] - regs[reg3];
+			break;
+		case OP_DIV:
+			regs[reg1] = regs[reg2] / regs[reg3];
+			break;
+		default:
+			printf("Unknown instruction %d\n", instrNum);
+	}
 }
  
 /* display all registers as 4-digit hexadecimal words */
@@ -65,27 +113,27 @@ void showRegs()
     int i;
     printf("regs = ");
 
-    for(i=0; i<NUM_REGS; i++)
-        printf( "%04X ", regs[ i ] );
+    for(i = 0; i < NUM_REGS; i++)
+        printf("%04X ", regs[i]);
 
     printf( "\n" );
 }
  
 void run()
 {
-    while(running)
-    {
-        showRegs();
-        int instr = fetch();
-        decode(instr);
-        eval();
-    }
-    showRegs();
+	while(running)
+	{
+		showRegs();
+		int instr = fetch();
+		decode(instr);
+		eval();
+	}
+	showRegs();
 }
  
-int main(int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
-      run();
-      return 0;
+	run();
+	return 0;
 }
 
